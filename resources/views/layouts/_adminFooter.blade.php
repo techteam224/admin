47c00d60c3230d00398bc4d7aded35955d4505a7
techteam224 <techteam2224@gmail.com>
2017-07-29 14:07:49 +0530

    secure_asset

--- resources/views/layouts/_adminFooter.blade.php
+++ resources/views/layouts/_adminFooter.blade.php
@@ -1,41 +1,41 @@
 <!-- Mainly scripts -->
-<script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
-<script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ secure_asset('js/jquery-3.1.1.min.js') }}"></script>
+<script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
 <!-- Flot -->
-<script src="{{ asset('js/plugins/flot/jquery.flot.js') }}"></script>
-<script src="{{ asset('js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
-<script src="{{ asset('js/plugins/flot/jquery.flot.spline.js') }}"></script>
-<script src="{{ asset('js/plugins/flot/jquery.flot.resize.js') }}"></script>
-<script src="{{ asset('js/plugins/flot/jquery.flot.pie.js') }}"></script>
-<script src="{{ asset('js/plugins/flot/jquery.flot.symbol.js') }}"></script>
-<script src="{{ asset('js/plugins/flot/curvedLines.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/flot/jquery.flot.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/flot/jquery.flot.spline.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/flot/jquery.flot.resize.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/flot/jquery.flot.pie.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/flot/jquery.flot.symbol.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/flot/curvedLines.js') }}"></script>
 
 <!-- Peity -->
-<script src="{{ asset('js/plugins/peity/jquery.peity.min.js') }}"></script>
-<script src="{{ asset('js/demo/peity-demo.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/peity/jquery.peity.min.js') }}"></script>
+<script src="{{ secure_asset('js/demo/peity-demo.js') }}"></script>
 
 <!-- Custom and plugin javascript -->
-<script src="{{ asset('js/inspinia.js') }}"></script>
-<script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
+<script src="{{ secure_asset('js/inspinia.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/pace/pace.min.js') }}"></script>
 
 <!-- jQuery UI -->
-<script src="{{ asset('js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 
 <!-- Jvectormap -->
-<script src="{{ asset('js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
-<script src="{{ asset('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
 
 <!-- Sparkline -->
-<script src="{{ asset('js/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
 
 <!-- Sparkline demo data  -->
-<script src="{{ asset('js/demo/sparkline-demo.js') }}"></script>
+<script src="{{ secure_asset('js/demo/sparkline-demo.js') }}"></script>
 
 <!-- ChartJS-->
-<script src="{{ asset('js/plugins/chartJs/Chart.min.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/chartJs/Chart.min.js') }}"></script>
 
 <script>
     $(document).ready(function() {
